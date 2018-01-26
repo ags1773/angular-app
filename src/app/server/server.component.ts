6940acc0917a1e701167ed49dd6f6445912dd8ca
amogh1773 <amogh1773@gmail.com>
2018-01-26 23:19:01 +0530

    initial commit

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,17 @@
+// import { Component } from '@angular/core';
+//
+// @Component({
+//   selector: 'app-server',
+//   templateUrl: './server.component.html'
+// })
+// export class ServerComponent { }
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-server',
+  templateUrl: './server.component.html'
+})
+export class ServerComponent {
+
+}
