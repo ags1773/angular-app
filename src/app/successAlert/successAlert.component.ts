fbcf1a7f7ea88c0c81c604b360b5c98527941e49
amogh1773 <amogh1773@gmail.com>
2018-01-27 00:20:05 +0530

    Adding components - assignment

---
+++ src/app/successAlert/successAlert.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: "app-success",
+  templateUrl: "./successAlert.component.html",
+  styles: [`p {
+    background-color: green;
+  }`]
+})
+
+export class SuccessAlertComponent{ }
