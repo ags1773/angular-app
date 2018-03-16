252f7fa0e547178ff5d059e2e9d91c3b1577f312
amogh1773 <amogh1773@gmail.com>
2018-03-16 19:24:03 +0530

    Services - Assignment solution

--- src/app/active-users/active-users.component.ts
+++ src/app/active-users/active-users.component.ts
@@ -1,15 +1,20 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UsersService } from '../users.service';
 
 @Component({
   selector: 'app-active-users',
   templateUrl: './active-users.component.html',
   styleUrls: ['./active-users.component.css']
 })
-export class ActiveUsersComponent {
-  @Input() users: string[];
-  @Output() userSetToInactive = new EventEmitter<number>();
+export class ActiveUsersComponent implements OnInit {
+  users: string[];
+
+  constructor(private usrService: UsersService){}
+  ngOnInit(){
+    this.users = this.usrService.activeUsers;
+  }
 
   onSetToInactive(id: number) {
-    this.userSetToInactive.emit(id);
+    this.usrService.setToInactive(id);
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,9 @@ import { AppComponent } from './app.component';
 import { ActiveUsersComponent } from './active-users/active-users.component';
 import { InactiveUsersComponent } from './inactive-users/inactive-users.component';
 
+import { UsersService } from './users.service';
+import { CounterService } from './counter.service';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -18,7 +21,9 @@ import { InactiveUsersComponent } from './inactive-users/inactive-users.componen
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [CounterService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(private ctrService: CounterService){}
+}
